da88ea5b7621b4e68c9c07daf2bde8e928909bb1
Ludvík Prokopec <ludvik.prokopec@gmail.com>
2025-01-25 20:39:46 +0100

    add effects

---
+++ src/effects/Effect.ts
@@ -0,0 +1,23 @@
+import { Pirate } from '../model/Pirate';
+import { Player } from '../model/Player';
+import { Ship } from '../model/Ship';
+
+export abstract class Effect {
+  active = true;
+
+  preEffect(_pirate: Pirate, _ship: Ship, _player: Player): void {}
+
+  abstract effect(pirate: Pirate, ship: Ship, player: Player): void;
+
+  postEffect(_pirate: Pirate, _ship: Ship, _player: Player): void {}
+
+  applyTo(pirate: Pirate, ship: Ship, player: Player): void {
+    if (!this.active) {
+      return;
+    }
+
+    this.preEffect(pirate, ship, player);
+    this.effect(pirate, ship, player);
+    this.postEffect(pirate, ship, player);
+  }
+}
---
+++ src/effects/SingleEffect.ts
@@ -0,0 +1,10 @@
+import { Pirate } from '../model/Pirate';
+import { Player } from '../model/Player';
+import { Ship } from '../model/Ship';
+import { Effect } from './Effect';
+
+export abstract class SingleEffect extends Effect {
+  postEffect(_pirate: Pirate, _ship: Ship, _player: Player): void {
+    this.active = false;
+  }
+}
